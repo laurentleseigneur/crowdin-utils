f8787cf1cb3c8ad63318c809aac1cbbd42c5fa08
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2021-06-01 15:13:47 +0200

    switch to maven

--- src/main/groovy/com/bonitasoft/localization/localizationUtil/LocalizationScanner.groovy
+++ src/main/groovy/com/bonitasoft/localization/localizationUtil/LocalizationScanner.groovy
@@ -20,23 +20,52 @@ public class LocalizationScanner {
 
     JsonSlurper jsonSlurper = new JsonSlurper()
 
-    def extractCrowdinKeys() {
-//        def allProps = [:]
+    def importCrowdinKeys() {
+       def all=[:]
+        getSupportedLanguages().each { language ->
+            println("get ${language} translations")
+            TreeMap props = new TreeMap()
+            def localeFilePath = "${basePath}/localization/locale_${language}.json"
+            def localFile = new File(localeFilePath)
+            def localeProps = readJsonPropertyFile(localFile)
+            localeProps.each { k, v ->
+                props.put(k, v)
+            }
+            def crowdinFile = new File("${basePath}/localization/localization_${language}_crowdin.json")
+            def crowdinProps = readJsonPropertyFile(crowdinFile)
+            crowdinProps.each { k, v ->
+                props.putIfAbsent(k, v)
+            }
+
+            all.put(language,props)
+        }
+        List<Object> localizationFiles = getLocalizationFiles()
+        localizationFiles.each {
+            writePropertiesToFile(it,all)
+        }
+    }
+
+    def exportCrowdinKeys() {
+        def allProps = [:]
+        def localeProps = [:]
         TreeMap keyUsage = new TreeMap()
         TreeMap allKeys = new TreeMap()
         List<Object> localizationFile = getLocalizationFiles()
         localizationFile.each { file ->
-            def props = readProps(file as File)
+            def props = readJsonPropertyFile(file as File)
             def filteredLanguages = props.findAll { language, values ->
                 isValidLanguage(language)
             }
             filteredLanguages.each { language, properties ->
-//                if (!allProps["${language}"]) {
-//                    allProps["${language}"] = new Properties()
-//                }
+                if (!allProps["${language}"]) {
+                    allProps["${language}"] = new Properties()
+                }
+                if (!localeProps["${language}"]) {
+                    localeProps["${language}"] = new Properties()
+                }
                 properties.each { entry, value ->
                     if (isValidProperty(entry)) {
-//                        (allProps["${language}"] as Properties).putIfAbsent(entry, value)
+                        (allProps["${language}"] as Properties).putIfAbsent(entry, value)
                         allKeys.putIfAbsent(entry, entry)
                         def usageLabel = "${(file as File).getCanonicalPath()} [${language}]"
                         if (!keyUsage.containsKey(entry)) {
@@ -44,36 +73,23 @@ public class LocalizationScanner {
                         } else {
                             keyUsage.get(entry).add(usageLabel)
                         }
+                    } else {
+                        (localeProps["${language}"] as Properties).putIfAbsent(entry, value)
                     }
                 }
             }
-//            allProps.each { language, properties ->
-//                allKeys.each { key, value ->
-//                    if (!properties.containsKey(key)) {
-//                        println("add missing key [${key}] for language [${language}]")
-//                        properties.put(key, key)
-//                    }
-//                }
-//            }
-//            allProps.each { language, properties ->
-//                def jsonFile = new File("${basePath}/localization/localization_${language}.json")
-//                jsonFile.text = JsonOutput.prettyPrint(JsonOutput.toJson(properties))
-//                def filteredProperties = (properties as Properties).findAll {
-//                    it.getKey().toString().startsWith('$')
-//                }
-//                def localeJson = new File("${basePath}/localization/local_${language}.json")
-//                localeJson.text = JsonOutput.prettyPrint(JsonOutput.toJson(filteredProperties))
-//            }
         }
-        writePropertiesToFile("localization.json", allKeys)
-        writePropertiesToFile("keyUsage.json", keyUsage)
+        localeProps.each { language, properties ->
+            writePropertiesToFile("${basePath}/localization/locale_${language}.json", properties)
+        }
+        writePropertiesToFile("${basePath}/localization/localization.json", allKeys)
+        writePropertiesToFile("${basePath}/localization/keyUsage.json", keyUsage)
     }
 
-    private void writePropertiesToFile(String fileName, TreeMap keys) {
-        def filePath = "${basePath}/localization/${fileName}"
-        def keysJson = new File(filePath)
+    private void writePropertiesToFile(def filePath, def keys) {
+        def keysJson = new File(filePath as String)
         keysJson.text = JsonOutput.prettyPrint(JsonOutput.toJson(keys))
-        println("write file ${filePath}")
+        println("writing file ${filePath}")
     }
 
 
@@ -104,7 +120,7 @@ public class LocalizationScanner {
         folderName.equals('web_page')
     }
 
-    def readProps(File file) {
+    def readJsonPropertyFile(File file) {
         println("parsing file ${file.getAbsolutePath()}")
         def props = jsonSlurper.parse(file)
         props
@@ -112,21 +128,13 @@ public class LocalizationScanner {
 
     boolean isValidLanguage(String language) {
         boolean isValid = Locale.getISOLanguages().contains(language.substring(0, 2))
-        def supportedLanguages = ['fr-FR', 'es-ES']
+        def supportedLanguages = getSupportedLanguages()
         isValid = isValid && supportedLanguages.contains(language)
         isValid
     }
 
-    def deploy() {
-        def list = []
-
-        def dir = new File(basePath)
-        dir.eachFileRecurse(FileType.FILES) { file ->
-            list << file
-        }
-        def filtered = list.findAll { file ->
-            isLocalizationFile(file as File)
-        }
+    private ArrayList<String> getSupportedLanguages() {
+        ['fr-FR', 'es-ES']
     }
 
     boolean isValidProperty(String property) {
--- src/main/groovy/com/bonitasoft/localization/localizationUtil/LocalizationUtilApplication.groovy
+++ src/main/groovy/com/bonitasoft/localization/localizationUtil/LocalizationUtilApplication.groovy
@@ -20,21 +20,28 @@ class LocalizationUtilApplication {
             println "Arguments:"
             args.each { println it }
             LocalizationScanner localizationScanner = new LocalizationScanner()
-            localizationScanner.setBasePath(args[0])
-            def action = args[1]
-            switch (action) {
-                case 'scan':
-                    localizationScanner.extractCrowdinKeys()
-                    break
-                case 'deploy':
-                    localizationScanner.deploy()
-                    break
+            if (args.size() != 2) {
+                println('usage: java -jar localizationUtil-<VERSION>.jar <FOLDER> [exportKeys|importKeys]')
+            } else {
+                localizationScanner.setBasePath(args[0])
+                def action = args[1]
+                switch (action) {
+                    case 'exportKeys':
+                        localizationScanner.exportCrowdinKeys()
+                        break
+                    case 'importKeys':
+                        localizationScanner.importCrowdinKeys()
+                        break
+                    default:
+                        println('usage: java -jar localizationUtil-<VERSION>.jar <FOLDER> [exportKeys|importKeys]')
+                }
             }
 
 
         }
 
     }
+
 }
 
 
